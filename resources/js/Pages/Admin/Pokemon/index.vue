338b8456eff0487d857d18e69c3bbcf385c07451
Thomas <t.copin@yahoo.fr>
2024-06-11 17:11:45 +0200

    little change to the css

--- resources/js/Pages/Admin/Pokemon/index.vue
+++ resources/js/Pages/Admin/Pokemon/index.vue
@@ -16,8 +16,8 @@ defineProps({
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Administation Pokemon</h2>
         </template>
         <div class="flex justify-center">
-            <div class="bg-gray-300 my-2 px-2 py-2 rounded-lg">
-                <a :href="route('pokemon.create')">Create</a>
+            <div class="my-3">
+                <a :href="route('pokemon.create')" class="bg-gray-300 px-2 py-2 rounded-lg">Create</a>
             </div>
         </div>
         

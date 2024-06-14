af79d88e8de713d8dd6eaf6b7818e123cad84c1d
Thomas <t.copin@yahoo.fr>
2024-06-14 14:14:08 +0200

    Admin apge complete

--- resources/js/Pages/Admin/index.vue
+++ resources/js/Pages/Admin/index.vue
@@ -15,13 +15,18 @@ import { Head } from '@inertiajs/vue3';
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                <div class="bg-white p-6 overflow-hidden shadow-sm sm:rounded-lg">
                         <p>Welcome on the administation Homepage</p>
-                        <p>When you create a new pokemon, you will just see the startings attacks.</p>
-                        <p>we recommend to set the level at 1, and leaving the input empty for the attacks not learned.</p>
-                        <p>You can add more attacks in the edit form when the pokemon is created</p>
-                    </div>
+                        
+                    
+                </div>
+                <div class="bg-white my-2 p-6 text-gray-900 dark:text-gray-100">
+                    <h3 class="flex justify-center py-5">Pokemon</h3>
+                    <p>When you create a new pokemon, you will just see the startings attacks.</p>
+                    <p>we recommend to set the level at 1, and leaving the input empty for the attacks not learned.</p>
+                    <p>You can add more attacks in the edit form when the pokemon is created</p>
+                    <p>The stats (hp, vit, attspe, ...) should be between 1 and 150</p>
+                    <p>You can add one attack to a pokemon many times(a pokemon can learn the same attack at different level)</p>
                 </div>
             </div>
         </div>
